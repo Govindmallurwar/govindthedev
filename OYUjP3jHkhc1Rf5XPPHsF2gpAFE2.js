e6bdfe5ebfc2406a1daaa6b5766b3234d96959ac
Govind Malurwar <govind.mallurwar@gmail.com>
2025-01-05 22:15:01 +0000

    Add files via upload

---
+++ OYUjP3jHkhc1Rf5XPPHsF2gpAFE2.js
@@ -0,0 +1,67 @@
+globalThis.cv = Object.assign(globalThis.cv || {}, {
+    "general": {
+      "profilePhoto": "https://firebasestorage.googleapis.com/v0/b/maitake-project.appspot.com/o/profilePhotos%2FOYUjP3jHkhc1Rf5XPPHsF2gpAFE2%2F22aad3fa-5770-4b90-b9f9-48aa7a44015f-sri.jpg?alt=media&token=6db3e84f-f3e7-445f-b904-1080a3191dee",
+      "username": "Govind",
+      "displayName": "Govind Malurwar",
+      "profession": "Senior Software Engineer",
+      "location": "Manchester UK",
+      "pronouns": null,
+      "byline": "Senior Software Engineer in Manchester UK",
+      "website": "https://govindthedev.sh",
+      "websiteURL": "https://govindthedev.sh",
+      "about": null,
+      "status": {
+        "text": "\n",
+        "emoji": "🙃",
+        "timestamp": "2021-01-04T22:08:04.906Z"
+      }
+    },
+    "projects": [],
+    "sideProjects": [],
+    "exhibitions": [],
+    "talks": [],
+    "writing": [],
+    "awards": [],
+    "features": [],
+    "workExperience": [],
+    "volunteering": [],
+    "education": [],
+    "certifications": [],
+    "contact": [
+      {
+        "id": "3KhrZn2inhSNIz1LVxvq",
+        "platform": "LinkedIn",
+        "handle": "sriganeshsureshkumar",
+        "url": "https://www.linkedin.com/in/govind-malurwar/"
+      },
+      {
+        "id": "a9mcCX9vUMCOrCJtnmw3",
+        "platform": "Open for: SWE roles",
+        "handle": "linkedin",
+        "url": "https://www.linkedin.com/in/govind-malurwar/"
+      }
+    ],
+    "writingTab": {
+      "pages": []
+    },
+    "allCollections": [
+      {
+        "name": "Contact",
+        "items": [
+          {
+            "id": "3KhrZn2inhSNIz1LVxvq",
+            "platform": "LinkedIn",
+            "handle": "sriganeshsureshkumar",
+            "url": "https://linkedin.com/in/sriganeshsureshkumar"
+          },
+          {
+            "id": "a9mcCX9vUMCOrCJtnmw3",
+            "platform": "Open for: SWE roles",
+            "handle": "linkedin",
+            "url": "https://linkedin.com/in/sriganeshsureshkumar"
+          }
+        ]
+      }
+    ]
+  });
+  
