0f05d72f7970815c8ea8397bce4430ec94a30c89
Govind Malurwar <govind.mallurwar@gmail.com>
2025-01-10 11:02:32 +0000

    Add files via upload

---
+++ aY76UrMx2INRS53ugwfU9sfaHrm1.js
@@ -0,0 +1,67 @@
+globalThis.cv = Object.assign(globalThis.cv || {}, {
+    "general": {
+      "profilePhoto": "https://firebasestorage.googleapis.com/v0/b/maitake-project.appspot.com/o/profilePhotos%2FOYUjP3jHkhc1Rf5XPPHsF2gpAFE2%2F22aad3fa-5770-4b90-b9f9-48aa7a44015f-sri.jpg?alt=media&token=6db3e84f-f3e7-445f-b904-1080a3191dee",
+      "username": "Govind",
+      "displayName": "Govind Malurwar",
+      "profession": "Senior Software Engineer",
+      "location": "Manchester UK",
+      "pronouns": null,
+      "byline": "Senior Software Engineer in Manchester UK",
+      "website": "https://govindmalurwar.dev",
+      "websiteURL": "https://govindmalurwar.dev",
+      "about": null,
+      "status": {
+        "text": "\n",
+        "emoji": "🙃",
+        "timestamp": "2025-01-06T22:08:04.906Z"
+      }
+    },
+    "projects": [],
+    "sideProjects": [],
+    "exhibitions": [],
+    "talks": [],
+    "writing": [],
+    "awards": [],
+    "features": [],
+    "workExperience": [],
+    "volunteering": [],
+    "education": [],
+    "certifications": [],
+    "contact": [
+      {
+        "id": "3KhrZn2inhSNIz1LVxvq",
+       "platform": "Open for: SWE roles",
+        "handle": "linkedin",
+        "url": "https://www.linkedin.com/in/govind-malurwar/"
+      },
+      {
+        "id": "a9mcCX9vUMCOrCJtnmw3",
+        "platform": "Resume",
+        "handle": "resume",
+        "url": "https://govindmalurwar.dev/CV%20-%20Govind%20Malurwar.pdf"
+      }
+    ],
+    "writingTab": {
+      "pages": []
+    },
+    "allCollections": [
+      {
+        "name": "Contact",
+        "items": [
+          {
+            "id": "3KhrZn2inhSNIz1LVxvq",
+             "platform": "Open for: SWE roles",
+            "handle": "linkedin",
+            "url": "https://linkedin.com/in/govind-malurwar/"
+          },
+          {
+            "id": "a9mcCX9vUMCOrCJtnmw3",
+            "platform": "Resume",
+            "handle": "resume",
+            "url": "https://govindmalurwar.dev/CV%20-%20Govind%20Malurwar.pdf"
+          }
+        ]
+      }
+    ]
+  });
+  
